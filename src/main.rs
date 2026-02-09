8d94850cf2a943dd5ce7aa53625ffb044ca0cacf
xen556 <xen61690@gmail.com>
2026-02-09 20:15:24 +0100

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-use xcap::Monitor;
+use clap::Parser;
 
 fn main() {
+    let args = Args::parse();
+
+    println!("Name: {}", args.name);
+    println!("Fullscreen: {}", args.fullscreen);
+    println!("Area: {}", args.area);
+}
+
+#[derive(Parser)]
+#[command(name = "MyShot Tool")]
+struct Args {
+    name: String,
+
+    #[arg(long)]
+    area: bool,
+
+    #[arg(long)]
+    fullscreen: bool,
 }
